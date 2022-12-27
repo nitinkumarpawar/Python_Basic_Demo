22ca074642a868681b5fe5e15edf0804826a3a32
nitinkumar7273 <98381694+nitinkumar7273@users.noreply.github.com>
2022-12-27 12:48:37 +0530

    Updated

---
+++ unlimited_params.py
@@ -0,0 +1,20 @@
+# def string_params(*params):
+#     print(len(params))
+#     print(type(params))
+#     print(params[0])
+#     return params
+#
+#
+# print(string_params("Nitin", "Pawar"))
+
+# def calculate(*params):
+#     if params[2] == "+":
+#         return params[0] + params[1]
+#     elif params[2] == "-":
+#         return params[0] - params[1]
+#     elif params[2] == "/":
+#         return params[0] / params[1]
+#     elif params[2] == "%":
+#         return params[0] % params[1]
+#
+# print(calculate(1, 2, "-"))
